bd7d3724bcfabf1dc92d186a2b086079039d0c46
nicolas <nicolascursino@gmail.com>
2022-04-04 21:41:47 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="form-signin">
+      <form>
+        <img className="mb-4" src="#" alt="#" width="72" height="57"/>
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div className="form-floating">
+          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me"/> Remember me
+          </label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+      </form>
+    </main>
   );
 }
 
