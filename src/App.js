78f5acb0ee75e9bee88a3ad21d93c89b4172e201
Gabriel Souza <azevedoedit@gmail.com>
2022-04-13 09:40:17 -0300

    add login route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
+
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Login/> }></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
