3b17aa1b13157dc7270fd9940c26e86007682edf
Gabriel Souza <azevedoedit@gmail.com>
2022-04-13 09:34:33 -0300

    create login component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
 import './App.css';
-
+import Login from './components/Login';
 function App() {
   return (
-    <main className="form-signin">
-      <form>
-        <img className="mb-4" src="#" alt="#" width="72" height="57"/>
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-        <div className="form-floating">
-          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-          <label htmlFor="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me"/> Remember me
-          </label>
-        </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-      </form>
-      <footer>
-          <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-      </footer>
-    </main>
+    <Login/>
   );
 }
 
