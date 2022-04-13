7ba370950359f154852a64aa34f09d54d82fec00
Gabriel Souza <azevedoedit@gmail.com>
2022-04-13 18:18:09 -0300

    Add fol component and pdf file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
+import Login from './components/Login';
+import Fol from "./components/Fol";
 
 function App() {
   return (
-    <main className="form-signin">
-      <form>
-        <img className="mb-4" src="#" alt="#" width="72" height="57"/>
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-        <div className="form-floating">
-          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-          <label htmlFor="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me"/> Remember me
-          </label>
-        </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-      </form>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Login/> }></Route>
+        <Route path='/fol' element={ <Fol/> }></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
