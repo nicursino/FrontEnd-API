1e4053edda7f6e63ece0ddfcfe75bea012e7e2b3
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-04-14 17:39:14 -0300

    Add css to fol component

--- src/components/Fol/index.js
+++ src/components/Fol/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-// import './pdf_view.css'
+import './fol.css';
 
 
 function Fol (){
+  const styles = {
+    page: {maxWidth: "100%"}
+  };
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -12,19 +15,21 @@ function Fol (){
   }
   return(
     <div>
-      <Document 
-      file="./assets/fol_teste.pdf" 
-      onLoadSuccess={onDocumentLoadSuccess}
-      onLoadError={console.error}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <div>
+      <div className="control">
         <button onClick={() => setPageNumber(pageNumber-1)} disabled={pageNumber <= 1}>{'<<'}</button>
         <button onClick={() => setPageNumber(pageNumber+1)} disabled={pageNumber >= numPages}>{'>>'}</button>
-        <p>
+      </div>
+      <div className="container">
+        <Document
+        file="./assets/fol_teste.pdf" 
+        onLoadSuccess={onDocumentLoadSuccess}
+        onLoadError={console.error}>
+          <Page style={styles.page} scale={0.9} pageNumber={pageNumber} />
+        </Document>
+      </div>
+      <p>
           Page {pageNumber} of {numPages}
         </p>
-      </div>
     </div>
   )
 }
